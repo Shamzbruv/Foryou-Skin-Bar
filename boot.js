64ab2e2dd70acd91cfa7b5bd1f04e28f62164947
Shamzbruv <98935666+Shamzbruv@users.noreply.github.com>
2026-06-25 21:35:50 -0500

    Enable customer account contrast safeguard on startup

--- boot.js
+++ boot.js
@@ -1,5 +1,6 @@
 require('./server-hotfix.js');
 require('./admin-nav-hotfix.js');
 require('./public-nav-hotfix.js');
+require('./customer-account-contrast-hotfix.js');
 require('./customer-portal-api-v2.js');
 require('./server.js');
