c314370a88f3371f56ccd4752eee4b1c93a01f26
Shamzbruv <98935666+Shamzbruv@users.noreply.github.com>
2026-06-25 23:02:30 -0500

    Enable full-height quiz experience on startup

--- boot.js
+++ boot.js
@@ -1,6 +1,7 @@
 require('./server-hotfix.js');
 require('./admin-nav-hotfix.js');
 require('./public-nav-hotfix.js');
+require('./quiz-layout-hotfix.js');
 require('./customer-account-contrast-hotfix.js');
 require('./customer-portal-api-v2.js');
 require('./server.js');
